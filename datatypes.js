c7fee7e2e4caf58380196166adfe47e92d2c1877
Sadhna1807 <sadhnaojha2003@gmail.com>
2025-08-08 14:29:09 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+"use strict";  // treat all js code as newer version
+// alert(3+3) we cant use it directly
+console.log(3+3);
+console.log("Sadhna");
+let name = "Sadhna"
+let age = 18
+let isloggin = false;
+// number => 2 to power 53
+// string =>" "
+// Boolean => true/false
+// null => standalone Value
+// symbol => unique
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
