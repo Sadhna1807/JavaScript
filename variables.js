1b43c88434ea71ac0cdc3805d73304e9d69a01ba
Sadhna1807 <sadhnaojha2003@gmail.com>
2025-08-08 14:28:47 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "Sad@google.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+// accountId = 2 // not allowed
+console.log(accountId);
+console.table([accountId,accountPass,accountCity])
