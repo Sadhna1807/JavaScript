4b856ba07fc839ac61a7261073a459514d6667eb
Sadhna1807 <sadhnaojha2003@gmail.com>
2025-08-09 22:34:31 +0530

    Add files via upload

---
+++ Operations.js
@@ -0,0 +1,26 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+console.log(2+3);
+console.log(2*2);
+console.log(2%2);
+let str1 = "hello"
+let str2 = "Sadhna"
+let str3 = str1+str2
+console.log(str3);
+console.log("1" +2);
+console.log("2"+1);
+console.log(1+2+"3");
+console.log((3+4)*5%4);
+console.log(+true);
+console.log("2">1);
+console.log(null ==0);
+
+
+
+
+
+
+
+
+
