a893d0f68342f28e694498a0e74943bdc8c1b4f6
Sadhna1807 <sadhnaojha2003@gmail.com>
2025-08-10 13:45:38 +0530

    Add files via upload

---
+++ looping.js
@@ -0,0 +1,44 @@
+// console.log("Hello I am condition tutorials")
+// let age = 45;
+// let grace = 2;
+// if(age+grace>18){
+//     console.log("you are drive");
+
+// }
+// else{
+//     console.log("you cant drive");
+// }
+// 
+// let a =1;
+// for (let index = 0; index < 100; index++) {
+//    console.log(a+index);
+    
+// }
+// let obj = {
+//     name : "Sadhna",
+//     role: "programmer",
+//     company:"salesforce"
+// }
+// for (const key in obj) {
+  
+//         const element = obj[key];
+//         console.log(key,element)
+        
+//     }
+    // let i =0;
+    // while(i<6){
+    //     console.log(i);
+    //     i++;
+    // }
+// function nice(name) {
+//     console.log("hey"+name+"you are nice!")
+//      console.log("hey"+name+"you are nice!")
+//       console.log("hey"+name+"you are good!")
+//        console.log("hey"+name+"your shirt is nice!")
+// }
+// nice("rohit")
+function sum(a,b) {
+    console.log(a+b)
+}
+sum(3,5)
+
