c4cece1db943fd4ae35642d2d3414049b0770041
Sadhna1807 <sadhnaojha2003@gmail.com>
2025-08-10 13:45:58 +0530

    Add files via upload

---
+++ typeof.js
@@ -0,0 +1,22 @@
+console.log("Hey this is tutorial 2")
+// var a = 5;
+// var b = 6;
+// var c = "Sadhna";
+// console.log(a+b+8)
+// console.log(typeof a, tyoeof ,typeof c);
+// let b = 6;
+// let c = "Sadhna";
+// let_a = "ojha";
+// console.log(a);
+// let x = "Sadhna";
+// let y = 22;
+// const p = true;
+// let q = undefined;
+// console.log(x,y,p,q)
+// console.log(typeof x,typeof y,typeof p,typeof q)
+let o = {
+    "name" : "sadhna",
+    "job role" : 3453
+   
+}
+ console.log(o);
