13a1f1a7e3dde30015338be0c45803150a7a63f5
Sadhna1807 <sadhnaojha2003@gmail.com>
2025-08-10 13:43:52 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,16 @@
+const name = "Sadhna"
+const repoCount = 50;
+console.log(name+repoCount);
+console.log(`hello my name is ${name} and my repo is ${repoCount}`);
+const gameName = new String('Sadhna')
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+
+
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+const anotherstring = gameName.slice(-8,4)
+console.log(anotherstring);
